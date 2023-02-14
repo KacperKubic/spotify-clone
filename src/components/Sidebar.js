fce18f9c10fdefb78f4c5c7d91cd82e6188a809c
KacperKubic <kacper.kubic2211@gmail.com>
2023-02-14 17:22:17 +0100

    Final commit
    I finished the entire app and added styling for mobile

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,21 +1,31 @@
 import { Home, LibraryMusic, Search } from "@mui/icons-material";
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import '../styles/Sidebar.css';
 import SidebarElement from "./SidebarElement";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 const Sidebar = ({ playlists }) => {
+    const [toggle, setToggle] = useState(false);
+    const handleClick = () => setToggle(!toggle)
+
     return(
-        <div className="sidebar">
-            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
-            <SidebarElement title="Home" Icon={Home}/>
-            <SidebarElement title="Search" Icon={Search} />
-            <SidebarElement title="Library" Icon={LibraryMusic} />
-            <strong className="sidebar-title">PLAYLISTS</strong>
-            <hr />
-            {playlists?.playlists?.items?.map((playlist) => {
-                return <SidebarElement key={playlist.id} playlist={playlist} />
-            })}
+        <div className={toggle ? "sidebar-active" : "sidebar"}>
+            <div className="toggled">
+                <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
+                {toggle ? <ArrowBackIosIcon className="sidebar-icon" onClick={handleClick}/> : <ArrowForwardIosIcon className="sidebar-icon" onClick={handleClick}/> }
+            </div>
+            <div className={toggle ? "sidebar-items" : "sidebar-items-active"}>
+                <SidebarElement title="Home" Icon={Home}/>
+                <SidebarElement title="Search" Icon={Search} />
+                <SidebarElement title="Library" Icon={LibraryMusic} />
+                <strong className="sidebar-title">PLAYLISTS</strong>
+                <hr />
+                {playlists?.playlists?.items?.map((playlist) => {
+                    return <SidebarElement key={playlist.id} playlist={playlist} />
+                })}
+            </div>
         </div>
     )
 }
